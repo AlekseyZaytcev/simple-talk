8958dabd0867a07c2b445ea63e75716232746309
Aleksey Zaytcev <aleksey2142.zaytcev@gmail.com>
2020-09-20 13:35:19 +0300

    Fix linters

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,6 +2,6 @@
 
 require 'i18n'
 
-I18n.load_path += Dir[Hanami.root.join("config/locales/**/*.{yml,rb}")]
+I18n.load_path += Dir[Hanami.root.join('config/locales/**/*.{yml,rb}')]
 I18n.default_locale = 'en'
 I18n.backend.load_translations
